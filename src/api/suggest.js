c1d92fc3121947e34f9656b213c2b11cbfe7c3f3
Legal Soft <legalsoft1@LAP-SEQ-LS-SC.local>
2025-07-21 15:06:21 -0700

    new

--- src/api/suggest.js
+++ src/api/suggest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://localhost:8080'
+const API_BASE_URL = 'https://orgchartbackend-production.up.railway.app/'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
